b82a63589136a33b01b3d68eb8e663078cf8d949
daniilkorytko <43876618+daniilkorytko@users.noreply.github.com>
2018-10-05 15:24:01 +0300

    Add files via upload

---
+++ Task3.1.ps1
@@ -0,0 +1,12 @@
+﻿$NewResourceGroup = "Task3.1"
+New-AzureRmResourceGroup -Name $NewResourceGroup -Location "East US"
+
+
+
+New-AzureRmResourceGroupDeployment -Name Task3.Net -ResourceGroupName $NewResourceGroup `
+  -TemplateUri https://raw.githubusercontent.com/daniilkorytko/Task3.1/master/VNet.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/daniilkorytko/Task3.1/master/VNetParametr.json
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $NewResourceGroup `
+  -TemplateUri https://raw.githubusercontent.com/daniilkorytko/Task3.1/master/Storage.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/daniilkorytko/Task3.1/master/StorageParametrs.json
---
+++ Task3.2.ps1
@@ -0,0 +1,6 @@
+﻿$NewResourceGroup = "Task3.2"
+New-AzureRmResourceGroup -Name $NewResourceGroup -Location "East US"
+
+
+New-AzureRmResourceGroupDeployment -Name FinalTemplate -ResourceGroupName $NewResourceGroup `
+  -TemplateUri https://raw.githubusercontent.com/daniilkorytko/Task3.1/master/FinalTemplate.json
